d9b714db4f1f1758e64fabcfce696d10582da205
tatjapan <tatsuya.tsuri@gmail.com>
2021-10-27 11:24:38 +0900

    Changed to function

--- pages/contact-page.js
+++ pages/contact-page.js
@@ -1,6 +1,6 @@
 import Layout from "../components/Layout";
 import Image from "next/dist/client/image";
-const Contact = () => {
+export default function Contact() {
   return (
     <Layout title="Contact">
       <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
@@ -78,6 +78,4 @@ const Contact = () => {
       </div>
     </Layout>
   );
-};
-
-export default Contact;
+}
